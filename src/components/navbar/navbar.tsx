57bf1dcefa825f5a1587d5fa7d3d0cc04fe29410
Diego Prestamo <diegoprestamo@Diegos-MacBook-Pro-2.local>
2025-02-22 19:38:38 -0500

    changed colors on home page and added navbar

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,26 +3,20 @@ import Link from 'next/link';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="w-full px-6 py-4 border-b border-gray-200">
+    <nav className="w-full px-6 py-4 border-b-2 border-gray-700 bg-[#eae6df]">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Logo/Brand */}
-        <Link href="/" className="text-xl font-semibold">
-          RESEDA PHOTOGRAPHY
+        <Link href="/" className="text-xl font-semibold text-[#d37354]">
+          CUSEHACKS
         </Link>
 
         {/* Navigation Links */}
         <div className="flex gap-8">
-          <Link href="/services" className="hover:text-gray-600">
-            SERVICES
+          <Link href="/services" className="text-black hover:text-gray-600">
+            PAGE 1
           </Link>
-          <Link href="/gallery" className="hover:text-gray-600">
-            GALLERY
-          </Link>
-          <Link href="/information" className="hover:text-gray-600">
-            INFORMATION
-          </Link>
-          <Link href="/schedule" className="hover:text-gray-600">
-            SCHEDULE A SHOOT
+          <Link href="/gallery" className="text-black hover:text-gray-600">
+            PAGE 2
           </Link>
         </div>
       </div>
